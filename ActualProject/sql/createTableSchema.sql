a21db8d9249af481148bf0a27b1a71e8d0d3b64a
marcusngwj <marcusngwj@msn.com>
2018-09-18 17:33:55 +0800

    Update db schema integrity constraints

--- ActualProject/sql/createTableSchema.sql
+++ ActualProject/sql/createTableSchema.sql
@@ -7,22 +7,24 @@ CREATE TABLE member (
 CREATE TABLE project (
 	id NUMERIC(16) PRIMARY KEY CHECK(id >=0),
 	title VARCHAR(256) NOT NULL,
-	description VARCHAR(2048) DEFAULT '',
-	category VARCHAR(256) CHECK(category='Business' OR category='IT' ),
-	start_date DATE DEFAULT CURRENT_DATE NOT NULL,
-	duration TIME DEFAULT '00:00:00' NOT NULL
+	description VARCHAR(2048) DEFAULT '' NOT NULL,
+	category VARCHAR(256) NOT NULL CHECK(category='Fashion' OR category='Technology' OR category='Games' OR category='Food' OR category='Music' OR category='Photography' OR category='Handicraft' OR category='Community'),
+	start_date DATE NOT NULL DEFAULT CURRENT_DATE,
+	duration TIME NOT NULL DEFAULT '00:00:00'
 );
 
 CREATE TABLE advertise (
-	entrepreneur VARCHAR(16) REFERENCES member(username) ON DELETE CASCADE,
-	proj_id NUMERIC(16) REFERENCES project(id) CHECK(proj_id >=0),
-	amount NUMERIC(15,2) DEFAULT '0.00' NOT NULL CHECK(amount >= 0),
+	entrepreneur VARCHAR(16) REFERENCES member(username),
+	proj_id NUMERIC(16) REFERENCES project(id),
+	amt_needed NUMERIC(15,2) NOT NULL DEFAULT '0.00' CHECK(amt_needed >= 0),
+	amt_raised NUMERIC(15,2) NOT NULL DEFAULT '0.00') CHECK(amt_raised >= amt_needed),
+	status INT NOT NULL CHECK(status=0 OR status=1),
 	PRIMARY KEY(entrepreneur, proj_id)
 );
 
 CREATE TABLE fund (
-	investor VARCHAR(16) REFERENCES member(username) ON DELETE CASCADE,
-	proj_id NUMERIC(16) REFERENCES project(id) CHECK(proj_id >=0),
-	amount NUMERIC(15,2) DEFAULT '0.00' NOT NULL CHECK(amount >= 0),
-	status INT NOT NULL CHECK(status=0 OR status=1)
+	investor VARCHAR(16) REFERENCES member(username),
+	proj_id NUMERIC(16) REFERENCES project(id),
+	amount NUMERIC(15,2) NOT NULL DEFAULT '0.00' CHECK(amount >= 0),
+	PRIMARY KEY(investor, proj_id)
 );
