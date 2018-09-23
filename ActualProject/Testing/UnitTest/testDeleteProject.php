989ca9bfe21ce736ccab24a9c52b7730b2ad9e9d
marcusngwj <marcusngwj@msn.com>
2018-09-23 18:53:43 +0800

    Make use of connection to db file header

--- ActualProject/Testing/UnitTest/testDeleteProject.php
+++ ActualProject/Testing/UnitTest/testDeleteProject.php
@@ -1,17 +1,10 @@
 <?php
+    include './connectionToDatabase.php';
     include './displayTable.php';
 
     include '../../php/deleteProject.php';
-	// Connect to the database. Please change the password in the following line accordingly
-    $db = pg_connect("host=localhost port=5432 dbname=cs2102 user=postgres password=group18@CS2102");
 	
-	// Delete all tables from database - a clean start
-	$clear_database = file_get_contents('../../sql/dropTable.sql', true);
-	$database_cleaning_result = pg_query($db, $clear_database);
-	
-	// Create table in database
-	$table_schema = file_get_contents('../../sql/createTableSchema.sql', true);
-	$table_creation_result = pg_query($db, $table_schema);
+    $db = getDB();
     
     // Insert a project
     pg_query($db, "INSERT INTO project(title, description, category) VALUES('Chasing a girl named Sakura', 'A quest that Naruto sets out for', 'Community')");
