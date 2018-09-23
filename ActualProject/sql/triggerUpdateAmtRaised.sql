607d28ea676dec4f1f8fc0c6b1510268f17cab5f
marcusngwj <marcusngwj@msn.com>
2018-09-23 10:06:24 +0800

    Update trigger when updating and inserting into investment

--- ActualProject/sql/triggerUpdateAmtRaised.sql
+++ ActualProject/sql/triggerUpdateAmtRaised.sql
@@ -1,33 +1,47 @@
-CREATE OR REPLACE FUNCTION update_amt_raised_when_insert()
+CREATE OR REPLACE FUNCTION update_amt_raised_when_insert_invest()
 RETURNS TRIGGER AS $$
+DECLARE 
+    amtNeeded NUMERIC(15,2);
+    currentAmtRaised NUMERIC(15,2);
+    resultingAmtRaised NUMERIC(15,2);
 BEGIN
-IF (SELECT amt_raised FROM advertise WHERE NEW.proj_id = proj_id) + NEW.amount <= (SELECT amt_needed FROM advertise WHERE NEW.proj_id = proj_id) THEN
-    UPDATE advertise SET amt_raised=NEW.amount + amt_raised WHERE NEW.proj_id = proj_id;
-    RETURN NEW;
-ELSE
-    RAISE 'Your investment has exceeded the amount needed. PLease reduce.';
-    RETURN NULL;
-END IF;
+    amtNeeded = (SELECT amt_needed FROM advertise WHERE proj_id = NEW.proj_id);
+    currentAmtRaised = (SELECT amt_raised FROM advertise WHERE proj_id = NEW.proj_id);
+    IF currentAmtRaised + NEW.amount <= amtNeeded THEN
+        UPDATE advertise SET amt_raised=NEW.amount + amt_raised WHERE NEW.proj_id = proj_id;
+        RETURN NEW;
+    ELSE
+        RAISE 'Your investment has exceeded the amount needed. PLease reduce.';
+        RETURN NULL;
+    END IF;
 END; $$ LANGUAGE PLPGSQL;
 
-CREATE TRIGGER trigger_for_insert
+CREATE TRIGGER trigger_update_amt_raised_when_insert_invest
 BEFORE INSERT ON invest 
 FOR EACH ROW
-EXECUTE PROCEDURE update_amt_raised_when_insert();
+EXECUTE PROCEDURE update_amt_raised_when_insert_invest();
 
 
-
-/*CREATE OR REPLACE FUNCTION for_update()
+CREATE OR REPLACE FUNCTION update_amt_raised_when_update_invest()
 RETURNS TRIGGER AS $$
+DECLARE 
+    amtNeeded NUMERIC(15,2);
+    currentAmtRaised NUMERIC(15,2);
+    resultingAmtRaised NUMERIC(15,2);
 BEGIN
-UPDATE advertise SET status=1 WHERE OLD.amt_needed = NEW.amt_raised + OLD.amt_raised;
-RETURN NULL;
+    amtNeeded = (SELECT amt_needed FROM advertise WHERE proj_id = NEW.proj_id);
+    currentAmtRaised = (SELECT amt_raised FROM advertise WHERE proj_id = NEW.proj_id);
+    resultingAmtRaised = currentAmtRaised - OLD.amount + NEW.amount;
+    IF (resultingAmtRaised > amtNeeded) THEN
+        RAISE 'Your investment exceed the amount needed. Please reduce.';
+        RETURN NULL;
+    ELSE
+        UPDATE advertise SET amt_raised=(amt_raised-OLD.amount+NEW.amount) WHERE NEW.proj_id = proj_id;
+        RETURN NEW;
+    END IF;
 END; $$ LANGUAGE PLPGSQL;
 
-CREATE TRIGGER trigger_for_update
-BEFORE UPDATE
-on advertise
+CREATE TRIGGER trigger_update_amt_raised_when_update_invest
+BEFORE UPDATE ON invest 
 FOR EACH ROW
-WHEN (OLD.amt_needed = NEW.amt_raised + OLD.amt_raised)
-EXECUTE PROCEDURE for_update();
-*/
+EXECUTE PROCEDURE update_amt_raised_when_update_invest();
