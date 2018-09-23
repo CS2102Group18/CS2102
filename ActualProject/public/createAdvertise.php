005a8c0a8dddb47dbbfb0c77b3e288abeb729132
DESKTOP-I8I0R59\limweijie <limweijie1994@gmail.com>
2018-09-23 19:15:36 +0800

    Update api of createMember, createProject, createAdvertise

--- ActualProject/public/createAdvertise.php
+++ ActualProject/public/createAdvertise.php
@@ -1,4 +1,4 @@
 <?php
-  function createMember($db, $username, $password, $is_admin) {
+  function createAdvertise($db, $username, $password, $is_admin) {
     $sqlInsert = pg_query($db, "INSERT INTO member(username, password, is_admin) VALUES ('$_POST[username]', '$_POST[password]', '0' )");
 ?>
