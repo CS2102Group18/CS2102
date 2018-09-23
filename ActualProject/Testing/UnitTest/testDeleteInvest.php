d0a04c2b1b7a87a9fe9c27f890e0a5416f174d8a
marcusngwj <marcusngwj@msn.com>
2018-09-23 18:15:52 +0800

    update path for other test filtes

--- ActualProject/Testing/UnitTest/testDeleteInvest.php
+++ ActualProject/Testing/UnitTest/testDeleteInvest.php
@@ -6,11 +6,11 @@
     $db = pg_connect("host=localhost port=5432 dbname=cs2102 user=postgres password=group18@CS2102");
 	
 	// Delete all tables from database - a clean start
-	$clear_database = file_get_contents('../sql/dropTable.sql', true);
+	$clear_database = file_get_contents('../../sql/dropTable.sql', true);
 	$database_cleaning_result = pg_query($db, $clear_database);
 	
 	// Create table in database
-	$table_schema = file_get_contents('../sql/createTableSchema.sql', true);
+	$table_schema = file_get_contents('../../sql/createTableSchema.sql', true);
 	$table_creation_result = pg_query($db, $table_schema);
     
     // Insert members
