b2375df885d8ba74570078d06e782871dc19a933
Gautam Rajulu <43273161+gautamrajulu@users.noreply.github.com>
2018-10-10 21:32:47 +0800

    Add files via upload

---
+++ ActualProject/testing/INSERT INTO invest.sql
@@ -0,0 +1,268 @@
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Jose', 2, 100);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Jose', 4, 100);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Jose', 20, 100);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Christine', 4, 200);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Christine', 5, 200);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Christine', 12, 200);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Ignacio', 11, 300);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Ignacio', 14, 300);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Ignacio', 18, 300);
+
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Genevieve', 2, 400);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Genevieve', 16, 400);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Genevieve', 17, 400);
+
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Shaun', 1, 500);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Shaun', 7, 500);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Shaun', 14, 500);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Andre', 2, 600);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Andre', 5, 600);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Andre', 15, 600);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Perry', 5, 700);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Perry', 19, 700);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Lana', 12, 800);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Lana', 9, 800);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Lana', 6, 800);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Hope', 1, 900);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Hope', 6, 900);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Hope', 17, 900);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Diana', 5, 1000);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Diana', 15, 1000);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Diana', 20, 1000);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Patsy', 12, 1100);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Patsy', 4, 1100);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Patsy', 18, 1100);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Phil', 1, 1200);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Phil', 6, 1200);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Phil', 16, 1200);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Brandy', 4, 1300);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Brandy', 9, 1300);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Brandy', 18, 1300);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Flora', 2, 1400);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Flora', 5, 1400);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Flora', 8, 1400);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Gilbert', 12, 1500);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Gilbert', 4, 1500);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Gilbert', 7, 1500);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Ed', 12, 1600);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Ed', 5, 1600);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Ed', 14, 1600);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Tom', 12, 1700);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Tom', 15, 1700);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Tom', 2, 1700);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Gerald', 15, 1800);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Gerald', 18, 1800);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Gerald', 1, 1800);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Darrell', 12, 1900);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Darrell', 18, 1900);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Darrell', 16, 1900);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Jaqueline', 4, 2000);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Jaqueline', 8, 2000);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Jaqueline', 12, 2000);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Judy', 18, 2100);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Judy', 15, 2100);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Judy', 4, 2100);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Guadalupe', 12, 2200);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Guadalupe', 14, 2200);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Guadalupe', 15, 2200);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Marshall', 14, 2300);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Marshall', 19, 2300);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Marshall', 12, 2300);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Tim', 2, 2400);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Tim', 5, 2400);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Tim', 15, 2400);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Ethel', 15, 2500);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Ethel', 12, 2500);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Ethel', 2, 2500);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Roxanne', 4, 2600);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Roxanne', 19, 2600);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Roxanne', 12, 2600);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Jake', 18, 2700);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Jake', 8, 2700);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Jake', 19, 2700);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Virginia', 15, 2800);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Virginia', 6, 2800);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Virginia', 19, 2800);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Randall', 13, 2900);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Randall', 15, 2900);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Randall', 17, 2900);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Adrian', 7, 3000);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Adrian', 9, 3000);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Adrian', 12, 3000);
---
+++ ActualProject/testing/INSERT INTO project.sql
@@ -0,0 +1,79 @@
+INSERT INTO project (id, title, category)
+VALUES (1, 'Hello World', 'Technology');
+
+INSERT INTO project (id, title, category)
+VALUES (2, 'Bye World', 'Technology');
+
+INSERT INTO project (id, title, category)
+VALUES (3, 'Mario Super World', 'Games');
+
+INSERT INTO project (id, title, category)
+VALUES (4, 'Jelly Bean Mania', 'Food');
+
+INSERT INTO project (id, title, category)
+VALUES (5, 'Metal Pianos', 'Music');
+
+INSERT INTO project (id, title, category)
+VALUES (6, 'Lens Holders', 'Photography');
+
+INSERT INTO project (id, title, category)
+VALUES (7, 'Homemade Pokeballs', 'Handicraft');
+
+INSERT INTO project (id, title, category)
+VALUES (8, 'Community Hangout', 'Community');
+
+INSERT INTO project (id, title, category)
+VALUES (9, 'Bright Blue Dresses', 'Fashion');
+
+INSERT INTO project (id, title, category)
+VALUES (10, 'Thumbdrive Reader', 'Technology');
+
+INSERT INTO project (id, title, category)
+VALUES (11, 'Note Reader', 'Music');
+
+INSERT INTO project (id, title, category)
+VALUES (12, 'Wooden Baseball Bats', 'Handicraft');
+
+INSERT INTO project (id, title, category)
+VALUES (13, 'Dark Red Dresses', 'Fashion');
+
+INSERT INTO project (id, title, category)
+VALUES (14, 'Pokemon Themed Cafe', 'Food');
+
+INSERT INTO project (id, title, category)
+VALUES (15, 'Camera Sling', 'Photography');
+
+INSERT INTO project (id, title, category)
+VALUES (16, 'Tabletop Diceholder', 'Games');
+
+INSERT INTO project (id, title, category)
+VALUES (17, 'Github Magazine', 'Technology');
+
+INSERT INTO project (id, title, category)
+VALUES (18, 'Beethovens Greatest Hits', 'Music');
+
+INSERT INTO project (id, title, category)
+VALUES (19, 'Wooden Flasks', 'Handicraft');
+
+INSERT INTO project (id, title, category)
+VALUES (20, 'Kinetic Jacket', 'Fashion');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
