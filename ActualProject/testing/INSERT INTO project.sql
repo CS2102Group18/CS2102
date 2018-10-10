099098b1d25ddc5871ae30acf8a48f7610129581
Dylan Sng <dylansyj@gmail.com>
2018-10-11 01:10:10 +0800

    Revert "no message"
    This reverts commit d7cb3dee50272dbf0a5229b856dbef09603994a4.

--- ActualProject/testing/INSERT INTO project.sql
+++ ActualProject/testing/INSERT INTO project.sql
@@ -1,67 +1,79 @@
-INSERT INTO project (id, title, description, category)
-VALUES (1, 'Hello World', 'The waves were crashing on the shore; it was a lovely sight.', 'Technology');
+INSERT INTO project (id, title, category)
+VALUES (1, 'Hello World', 'Technology');
 
-INSERT INTO project (id, title, description, category)
-VALUES (2, 'Bye World', 'There were white out conditions in the town; subsequently, the roads were impassable.
-','Technology');
+INSERT INTO project (id, title, category)
+VALUES (2, 'Bye World', 'Technology');
 
-INSERT INTO project (id, title, description, category)
-VALUES (3, 'Mario Super World', 'This is the last random sentence I will be writing and I am going to stop mid-sent
-','Games');
+INSERT INTO project (id, title, category)
+VALUES (3, 'Mario Super World', 'Games');
 
-INSERT INTO project (id, title, description, category)
-VALUES (4, 'Jelly Bean Mania', 'She always speaks to him in a loud voice.
-','Food');
+INSERT INTO project (id, title, category)
+VALUES (4, 'Jelly Bean Mania', 'Food');
 
-INSERT INTO project (id, title, description, category)
-VALUES (5, 'Metal Pianos','The memory we used to share is no longer coherent.
-', 'Music');
+INSERT INTO project (id, title, category)
+VALUES (5, 'Metal Pianos', 'Music');
 
-INSERT INTO project (id, title, description, category)
-VALUES (6, 'Lens Holders','I love eating toasted cheese and tuna sandwiches.
-', 'Photography');
+INSERT INTO project (id, title, category)
+VALUES (6, 'Lens Holders', 'Photography');
 
-INSERT INTO project (id, title, description, category)
-VALUES (7, 'Homemade Pokeballs','He said he was not there yesterday; however, many people saw him there.
-', 'Handicraft');
+INSERT INTO project (id, title, category)
+VALUES (7, 'Homemade Pokeballs', 'Handicraft');
 
-INSERT INTO project (id, title, description, category)
-VALUES (8, 'Community Hangout','He didn’t want to go to the dentist, yet he went anyway.
-','Community');
+INSERT INTO project (id, title, category)
+VALUES (8, 'Community Hangout', 'Community');
 
-INSERT INTO project (id, title, description, category)
-VALUES (9, 'Bright Blue Dresses','Every element that you need in a product comes built in as a component.
-', 'Fashion');
+INSERT INTO project (id, title, category)
+VALUES (9, 'Bright Blue Dresses', 'Fashion');
 
-INSERT INTO project (id, title, description, category)
-VALUES (10, 'Thumbdrive Reader','All components fit perfectly with each other and can take variations in colour.', 'Technology');
+INSERT INTO project (id, title, category)
+VALUES (10, 'Thumbdrive Reader', 'Technology');
 
-INSERT INTO project (id, title, description, category)
-VALUES (11, 'Note Reader','Putting together a page has never been easier than matching together sections.', 'Music');
+INSERT INTO project (id, title, category)
+VALUES (11, 'Note Reader', 'Music');
 
-INSERT INTO project (id, title, description, category)
-VALUES (12, 'Wooden Baseball Bats',' From team presentation to pricing options, you can easily customise and built your pages.', 'Handicraft');
+INSERT INTO project (id, title, category)
+VALUES (12, 'Wooden Baseball Bats', 'Handicraft');
 
-INSERT INTO project (id, title, description, category)
-VALUES (13, 'Dark Red Dresses','If you want to get inspiration or just show something directly to your clients', 'Fashion');
+INSERT INTO project (id, title, category)
+VALUES (13, 'Dark Red Dresses', 'Fashion');
 
-INSERT INTO project (id, title, description, category)
-VALUES (14, 'Pokemon Themed Cafe',' you can jump start your development with our pre-built example pages.', 'Food');
+INSERT INTO project (id, title, category)
+VALUES (14, 'Pokemon Themed Cafe', 'Food');
 
-INSERT INTO project (id, title, description, category)
-VALUES (15, 'Camera Sling','We re-styled every Bootstrap element to make it resemble Material Design and also fit with each other. ','Photography');
+INSERT INTO project (id, title, category)
+VALUES (15, 'Camera Sling', 'Photography');
 
-INSERT INTO project (id, title, description, category)
-VALUES (16, 'Tabletop Diceholder','All the Bootstrap components that you need in a development have been re-design with the new look.','Games');
+INSERT INTO project (id, title, category)
+VALUES (16, 'Tabletop Diceholder', 'Games');
 
-INSERT INTO project (id, title, description, category)
-VALUES (17, 'Github Magazine','Besides the numerous basic elements, we have also created additional classes.', 'Technology');
+INSERT INTO project (id, title, category)
+VALUES (17, 'Github Magazine', 'Technology');
 
-INSERT INTO project (id, title, description, category)
-VALUES (18, 'Beethovens Greatest Hits','All these items will help you take your project to the next level.', 'Music');
+INSERT INTO project (id, title, category)
+VALUES (18, 'Beethovens Greatest Hits', 'Music');
 
-INSERT INTO project (id, title, description, category)
-VALUES (19, 'Wooden Flasks','We love cards and everybody on the web seems to.', 'Handicraft');
+INSERT INTO project (id, title, category)
+VALUES (19, 'Wooden Flasks', 'Handicraft');
 
-INSERT INTO project (id, title, description, category)
-VALUES (20, 'Kinetic Jacket','We have gone above and beyond with options for you to organise your information.', 'Fashion');
+INSERT INTO project (id, title, category)
+VALUES (20, 'Kinetic Jacket', 'Fashion');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
