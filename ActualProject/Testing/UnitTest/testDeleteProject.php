5b0e5efe63b85e8954dd42f3924b6a27af372bdd
marcusngwj <marcusngwj@msn.com>
2018-10-12 01:02:33 +0800

    Test delete project

--- ActualProject/Testing/UnitTest/testDeleteProject.php
+++ ActualProject/Testing/UnitTest/testDeleteProject.php
@@ -2,13 +2,25 @@
     include './connectionToDatabase.php';
     include './displayTable.php';
 
-    include '../../php/deleteProject.php';
+    include '../../php/deleteAdvertisedProject.php';
 	
     $db = getDB();
     
-    // Insert a project
-    pg_query($db, "INSERT INTO project(title, description, category) VALUES('Chasing a girl named Sakura', 'A quest that Naruto sets out for', 'Community')");
-    pg_query($db, "INSERT INTO project(title, description, category) VALUES('A Rogue Ninja', 'Sasuke finding his own identity', 'Games')");
+    // Insert members
+    pg_query($db, "INSERT INTO member(username, password) VALUES('Naruto', '12r42345f')");
+    pg_query($db, "INSERT INTO member(username, password) VALUES('Sasuke', 'wg453g25')");
+    pg_query($db, "INSERT INTO member(username, password) VALUES('Sakura', 'qwe342b')");
+    pg_query($db, "INSERT INTO member(username, password) VALUES('Ino', 'u45b3456h')");
+    pg_query($db, "INSERT INTO member(username, password) VALUES('Shikamaru', 'g3423g45')");
+    
+    // Insert projects
+    pg_query($db, "INSERT INTO advertised_project(entrepreneur, title, description, category, amt_needed) VALUES('Naruto', 'Chasing a girl named Sakura', 'A quest that Naruto sets out for', 'Community', 500)");
+    pg_query($db, "INSERT INTO advertised_project(entrepreneur, title, description, category, amt_needed) VALUES('Sasuke', 'A Rogue Ninja', 'Sasuke finding his own identity', 'Games', 1000)");
+    
+    // Insert investments
+    pg_query($db, "INSERT INTO invest (investor, proj_id, amount) VALUES ('Ino', 1, 100)");
+    pg_query($db, "INSERT INTO invest (investor, proj_id, amount) VALUES ('Shikamaru', 1, 50)");
+    pg_query($db, "INSERT INTO invest (investor, proj_id, amount) VALUES ('Sakura', 2, 90)");
     
     styleTable();
     
@@ -16,21 +28,26 @@
     echo "<br>";
     displayTableProject($db);
     echo "<br><br>";
+    displayTableInvest($db);
+    echo "<br><br>";
     echo "------------------------------------------------------------------------------------------------------";
     echo "<br><br>";
     
     
-    // Delete the member
+    // Delete the project
     $result_deletion = pg_query($db, deleteProject($db, 1));
     
     echo "<u><b>AFTER Deletion</b></u>";
     echo "<br>";
     displayTableProject($db);
     echo "<br><br>";
+    displayTableInvest($db);
+    echo "<br><br>";
     
     echo "<br><br>";
     echo "Things to note:";
     echo "<ul>";
-    echo "<li>Project with id=1 is removed from the table</li>";
+    echo "<li>Project with id=1 is removed from the TABLE advertised_project</li>";
+    echo "<li>Investment related to Project with id=1 is removed from the TABLE invest</li>";
     echo "</ul>";
 ?>  
