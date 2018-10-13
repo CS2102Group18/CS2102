903afc9d0c52d85f945cba593554ce180db7bc8d
marcusngwj <marcusngwj@msn.com>
2018-10-14 00:03:16 +0800

    update test

--- Testing/insertDummy.php
+++ Testing/insertDummy.php
@@ -1,14 +1,5 @@
 <?php
-    // Connect to the database. Please change the password in the following line accordingly
-    $db = pg_connect("host=localhost port=5432 dbname=cs2102 user=postgres password=group18@CS2102");
-
-    // Delete all tables from database - a clean start
-    $clear_database = file_get_contents('../sql/dropTable.sql', true);
-    $database_cleaning_result = pg_query($db, $clear_database);
-
-    // Create table in database
-    $table_schema = file_get_contents('../sql/createTableSchema.sql', true);
-    $table_creation_result = pg_query($db, $table_schema);
+    include 'initialiseWithEmptyTables.php';
 
     // Insert members
     pg_query($db, "INSERT INTO member(username, password, email, biography, is_admin) VALUES('admin', 'admin', 'admin@cs2102.com', 'This is an admin acount', 1)");
