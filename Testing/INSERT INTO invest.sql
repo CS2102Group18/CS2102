df772652d4aba22697a610a9ebb5046563ce09df
marcusngwj <marcusngwj@msn.com>
2018-10-13 23:37:21 +0800

    Bring out everything to parent directory Delete parent directory

---
+++ Testing/INSERT INTO invest.sql
@@ -0,0 +1,268 @@
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Jose', 2, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Jose', 4, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Jose', 20, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Christine', 4, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Christine', 5, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Christine', 12, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Ignacio', 11, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Ignacio', 14, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Ignacio', 18, 1);
+
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Genevieve', 2, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Genevieve', 16, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Genevieve', 17, 1);
+
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Shaun', 1, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Shaun', 7, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Shaun', 14, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Andre', 2, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Andre', 5, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Andre', 15, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Perry', 5, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Perry', 19, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Lana', 12, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Lana', 9, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Lana', 6, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Hope', 1, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Hope', 6, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Hope', 17, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Diana', 5, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Diana', 15, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Diana', 20, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Patsy', 12, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Patsy', 4, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Patsy', 18, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Phil', 1, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Phil', 6, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Phil', 16, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Brandy', 4, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Brandy', 9, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Brandy', 18, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Flora', 2, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Flora', 5, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Flora', 8, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Gilbert', 12, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Gilbert', 4, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Gilbert', 7, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Ed', 12, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Ed', 5, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Ed', 14, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Tom', 12, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Tom', 15, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Tom', 2, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Gerald', 15, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Gerald', 18, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Gerald', 1, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Darrell', 12, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Darrell', 18, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Darrell', 16, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Jacqueline', 4, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Jacqueline', 8, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Jacqueline', 12, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Judy', 18, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Judy', 15, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Judy', 4, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Guadalupe', 12, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Guadalupe', 14, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Guadalupe', 15, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Marshall', 14, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Marshall', 19, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Marshall', 12, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Tim', 2, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Tim', 5, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Tim', 15, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Ethel', 15, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Ethel', 12, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Ethel', 2, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Roxanne', 4, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Roxanne', 19, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Roxanne', 12, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Jake', 18, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Jake', 8, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Jake', 19, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Virginia', 15, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Virginia', 6, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Virginia', 19, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Randall', 13, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Randall', 15, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Randall', 17, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Adrian', 7, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Adrian', 9, 1);
+
+INSERT INTO invest (investor, proj_id, amount)
+VALUES ('Adrian', 12, 1);
